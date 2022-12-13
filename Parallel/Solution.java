96faf0b0b11fbf39f7764a7f77ce3ecdaba57e58
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2022-12-14 00:15:05 +0900

    평행

--- Parallel/Solution.java
+++ Parallel/Solution.java
@@ -12,6 +12,23 @@ class Solution {
     //기울기 = y값 증가량 / x값 증가량 = (y2 - y1) / (x2 - x1)
     public int solution(int[][] dots) {
         int answer = 0;
+        ArrayList<Double> arr = new ArrayList<>();
+        for (int i = 0; i < dots.length - 1; i++) {
+            if (answer == 1) {
+                break;
+            } else {
+                for (int j = i + 1; j < dots.length; j++) {
+                    double tmp = (double) (dots[i][1] - dots[j][1]) / (dots[i][0] - dots[j][0]);
+                    if (arr.contains(tmp)) {
+                        answer = 1;
+                        break;
+                    } else {
+                        arr.add(tmp);
+                    }
+
+                }
+            }
+        }
         return answer;
     }
 }
