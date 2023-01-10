532eb2d1d7d2254c2f933450bc16211d31f15be0
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2023-01-10 10:32:38 +0900

    콜라츠 추측

---
+++ Guessing_Collatz/Solution.java
@@ -0,0 +1,21 @@
+package Guessing_Collatz;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        long num = n;
+
+        while (num != 1) {
+            answer++;
+            if (answer == 500) {
+                return  -1;
+            }
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num = 3 * num + 1;
+            }
+        }
+        return answer;
+    }
+}
