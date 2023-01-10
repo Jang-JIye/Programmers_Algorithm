5b0087df432e5e323f3eab77d67d69280440baea
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2023-01-10 17:30:15 +0900

    없는 숫자 더하기

---
+++ Add_Misssing_Numbers/Solution.java
@@ -0,0 +1,18 @@
+package Add_Misssing_Numbers;
+/*
+0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다.
+numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+*/
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = -1;
+        int sum = 0; //숫자의 합을 저장
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i]; //배열 안의 모든 수의 합
+        }
+        sum = 45 - sum; // 0~9의 합 (45) - 배열 안의 모든 수의 합 = 배열에 없는 수의 합
+        answer = sum;
+        return answer;
+    }
+}
