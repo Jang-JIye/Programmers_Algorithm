1e60800b2cc1d4b47a199e4f82eaaba2111b34c6
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2023-01-11 11:50:10 +0900

    3진법 뒤집기

---
+++ Reverse_Ternary_System/Solution.java
@@ -0,0 +1,20 @@
+package Reverse_Ternary_System;
+/*
+3진법 뒤집기
+자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+*/
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        //배열로 변환
+        ArrayList<Integer> list = new ArrayList<>();
+        //10진법 -> 3진법
+        while (n != 0) {
+
+        }
+        //????
+        return answer;
+    }
+}
